677e14a00bb8845896303cd307aa4625a81641f8
j4mes <thanhduong020601@gmail.com>
2026-03-20 11:13:29 +0700

    adb

--- protoc.sh
+++ protoc.sh
@@ -10,8 +10,8 @@ git config user.email "$EMAIL"
 git fetch --all && git checkout main
 
 # sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
-# go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
-# go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
 protoc --go_out=./pb --go_opt=paths=source_relative \
   --go-grpc_out=./pb --go-grpc_opt=paths=source_relative \
  ./${SERVICE_NAME}/*.proto
